db23004f49a62fbc97b7b5b4c6f8cd995aca55f5
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-10-24 11:57:52 -0300

    Add pytest_addoption

--- conftest.py
+++ conftest.py
@@ -40,23 +40,55 @@ def pytest_runtest_call(item):
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    # command line options
+    max_loc = config.getoption("--max-loc")
+    max_if = config.getoption("--max-if")
+    max_for = config.getoption("--max-for")
+    max_while = config.getoption("--max-while")
+    max_elapsed_time = config.getoption("--max-elapsed-time")
+
+    # colors
     green = {"green": True, "bold": True}
     red = {"red": True, "bold": True}
     bold = {"bold": True}
+
     writer = terminalreporter
     writer.section("test measures")
     for item, values in MEASURES.items():
         writer.line("\n" + item, **bold)
         for measure, value in values.items():
             red_conditions = any([
-                measure == 'LOC' and value > 10,
-                measure == 'ifs_used' and value >= 1,
-                measure == 'for_used' and value >= 1,
-                measure == 'while_used' and value >= 1,
-                measure == 'elapsed_time' and value >= 5,
+                measure == 'LOC' and value >= max_loc,
+                measure == 'ifs_used' and value >= max_if,
+                measure == 'for_used' and value >= max_for,
+                measure == 'while_used' and value >= max_while,
+                measure == 'elapsed_time' and value >= max_elapsed_time,
             ])
 
             if red_conditions:
                 writer.line(f"{measure}: {value}", **red)
             else:
                 writer.line(f"{measure}: {value}", **green)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--max-loc", action="store", type=int, default=10,
+        help="Max line of code per test"
+    )
+    parser.addoption(
+        "--max-if", action="store", type=int, default=5,
+        help="Max quantity of if instructions per test"
+    )
+    parser.addoption(
+        "--max-for", action="store", type=int, default=10,
+        help="Max quantity of for instructions per test"
+    )
+    parser.addoption(
+        "--max-while", action="store", type=int, default=10,
+        help="Max quantity of while instructions per test"
+    )
+    parser.addoption(
+        "--max-elapsed-time", action="store", type=float, default=5,
+        help="Max elapsed time per test"
+    )
